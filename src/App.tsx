ce2399a3de4b1649510abe2c696ab2350c897dff
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-04-19 12:25:30 +0200

    usecontext-finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import UseContextComponent from "./UseContextComponent";
 import UseEffectComponent from "./UseEffectComponent";
 import UseStateComponent from "./UseStateComponent";
 
 function App() {
   return (
     <div>
-      <h1>useState</h1>
+      <h1>useContext</h1>
+      <UseContextComponent />
+      <h1>useEffect</h1>
       <UseEffectComponent />
+      <h1>useState</h1>
       <UseStateComponent />
     </div>
   );
