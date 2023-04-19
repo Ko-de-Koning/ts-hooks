598fba482bb178fa520e57606890ecd3d4bd3a27
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-04-19 14:04:08 +0200

    usereducer-finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import UseContextComponent from "./UseContextComponent";
 import UseEffectComponent from "./UseEffectComponent";
+import UseReducerComponent from "./UseReducerComponent";
 import UseStateComponent from "./UseStateComponent";
 
 function App() {
   return (
     <div>
+      <h1>useReducer</h1>
+      <UseReducerComponent />
       <h1>useContext</h1>
       <UseContextComponent />
       <h1>useEffect</h1>
---
+++ src/UseReducerComponent.tsx
@@ -0,0 +1,59 @@
+//if you have complex state this is usefull
+//when useState runs out of gas, that's when useReducer comes into play
+//redux built into react
+
+import { useReducer } from "react";
+
+const initialState = {
+  counter: 100,
+};
+
+type ACTIONTYPES =
+  | { type: "increment"; payload: number }
+  | { type: "decrement"; payload: number };
+
+function counterReducer(state: typeof initialState, action: ACTIONTYPES) {
+  switch (action.type) {
+    case "increment":
+      return { ...state, counter: state.counter + action.payload };
+    case "decrement":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      throw new Error("Dat kan niet");
+  }
+}
+
+function useReducerComponent() {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
+  return (
+    <div>
+      <div>{state.counter}</div>
+      <div>
+        <button
+          onClick={() =>
+            dispatch({
+              type: "increment",
+              payload: 10,
+            })
+          }
+        >
+          Increment
+        </button>{" "}
+        |
+        <button
+          onClick={() =>
+            dispatch({
+              type: "decrement",
+              payload: 5,
+            })
+          }
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default useReducerComponent;
