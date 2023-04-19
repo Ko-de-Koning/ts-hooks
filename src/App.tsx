9553b6ce4a140637f734f7b4be5870443c05e525
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-04-19 14:16:20 +0200

    feat:useref-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import UseContextComponent from "./UseContextComponent";
 import UseEffectComponent from "./UseEffectComponent";
 import UseReducerComponent from "./UseReducerComponent";
+import UseRefComponent from "./UseRefComponent";
 import UseStateComponent from "./UseStateComponent";
 
 function App() {
   return (
     <div>
+      <h1>useRef</h1>
+      <UseRefComponent />
       <h1>useReducer</h1>
       <UseReducerComponent />
       <h1>useContext</h1>
